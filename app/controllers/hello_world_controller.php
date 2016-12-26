9e24eb060b26def998940bf08adf598378dc6f94
Hannu Rantinoja <hannu.rantinoja@cs.helsinki.fi>
2016-12-26 12:44:17 +0200

    controller

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,18 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController {
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  echo 'Tämä on etusivu!';
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        //echo 'Tämä on etusivu!';
+        View::make("suunniteltmat/default.html");
+    } public static function user() {
+        View::make("suunniteltmat/user.html");
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      View::make("helloworld.html");
+    public static function sandbox() {
+        // Testaa koodiasi täällä
+        View::make("helloworld.html");
     }
-  }
+
+}
