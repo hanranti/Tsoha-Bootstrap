1c0bb5657594d396c3a29dea47a39db3cccc8b19
Rantinoja Hannu <hanranti@da5-cs-bk107-27.helsinki.fi>
2016-12-27 14:44:04 +0200

    sql

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,4 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Kayttaja (name, password) VALUES ('Kalle', 'Kalle123');
+INSERT INTO Kayttaja (name, password) VALUES ('Henri', 'Henri123');
+INSERT INTO Askare (name, info, deadline, tarkeysaste, kayttaja) VALUES ('Askare', 'tietoa', '2016-12-30', '10', 'Kalle');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,31 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Kayttaja(
+  name varchar(50) PRIMARY KEY NOT NULL,
+  password varchar(50) NOT NULL
+);
+CREATE TABLE Askare(
+  id SERIAL PRIMARY KEY,
+  name varchar(50) NOT NULL,
+  info varchar(50) NOT NULL,
+  deadline TIMESTAMP NOT NULL,
+  tarkeysaste INTEGER,
+  kayttaja varchar(50) REFERENCES Kayttaja
+);
+CREATE TABLE Luokka(
+  id SERIAL PRIMARY KEY,
+  name varchar(50) NOT NULL
+);
+CREATE TABLE Muistilista(
+  id SERIAL PRIMARY KEY,
+  name varchar(50) NOT NULL
+);
+CREATE TABLE AskareMuistilista(
+  id SERIAL PRIMARY KEY,
+  askareid INTEGER REFERENCES Askare(id),
+  muistilistaid INTEGER REFERENCES Muistilista(id)
+);
+CREATE TABLE MuistilistaLuokka(
+  id SERIAL PRIMARY KEY,
+  muistilistaid INTEGER REFERENCES Muistilista(id),
+  luokkaid INTEGER REFERENCES Luokka(id)
+);
